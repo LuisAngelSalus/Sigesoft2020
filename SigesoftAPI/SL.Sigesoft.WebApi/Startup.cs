d09a7086c79faccd2bafea41d1270a49570e286d
Pool Camacho <cgonzalesp@outlook.com>
2020-03-09 15:28:34 -0500

    udpate

--- SigesoftAPI/SL.Sigesoft.WebApi/Startup.cs
+++ SigesoftAPI/SL.Sigesoft.WebApi/Startup.cs
@@ -63,8 +63,6 @@ namespace SL.Sigesoft.WebApi
             services.AddScoped<IAccountSettingRepository, AccountSettingRepository>();
             services.AddScoped<IPasswordHasher<SystemUser>, PasswordHasher<SystemUser>>();            
             services.AddSingleton<TokenService>();
-            services.AddTransient<IEmailSender, EmailSender>();
-            services.Configure<EmailSenderOptions>(_configuration.GetSection("EmailSenderOptions"));
 
             //Accedemos a la sección JwtSettings del archivo appsettings.json
             var jwtSettings = _configuration.GetSection("JwtSettings");
